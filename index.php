c00e4b2f800bc42289f954b1413009a7318cd24f
Md Shayon <mdshayon0@gmail.com>
2019-01-30 03:17:16 +0600

    PHP basic. basic syntex

---
+++ index.php
@@ -0,0 +1,25 @@
+<!-- https://www.youtube.com/watch?v=pWG7ajC_OVo&list=PL4cUxeGkcC9gksOX3Kd9KPo-O68ncT05o -->
+<?php 
+    echo 'Hello World';
+    //echo "Hello";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Php file</title>
+</head>
+<body>
+    <h1> <?php echo "Hello world From php" ?> </h1>
+    <h1> There are some more files </h1>
+    <ul>
+        <li>basic/site.php</li>
+        <li>basic/site2.php</li>
+        <li>basic/site.php</li>
+    </ul>
+
+</body>
+</html>
