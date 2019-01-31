d41868da8ba31b5ee8019ecd4ec17ff32d0cd140
Md Shayon <mdshayon0@gmail.com>
2019-01-31 20:18:58 +0600

    url parameters

---
+++ site6.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Learning PHP</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container mt-5">
+    <form class="form-group" action="site6.php" method="get">
+        <input class="form-control" type="text" name="name">
+        <input class="btn btn-outline-success" value="submit" type="submit">
+    </form>
+
+    <?php 
+        echo "<h3 class='alert alert-primary'> $_GET[name] </h3> <br/>";
+        // http://localhost/basic/site6.php?name=Messi&age=34
+        // IF WE PASS &AGE=34 IN URL WE CAN ACCESS THAT FROM HERE
+        echo $_GET["age"];
+    ?>
+    </div>
+</body>
+</html>
