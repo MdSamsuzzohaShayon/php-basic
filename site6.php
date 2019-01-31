81c4829a2c67f91020c3c9a5dfcb2351b883a043
Md Shayon <mdshayon0@gmail.com>
2019-01-31 20:36:17 +0600

    get vs post

--- site6.php
+++ site6.php
@@ -9,16 +9,25 @@
 </head>
 <body>
     <div class="container mt-5">
-    <form class="form-group" action="site6.php" method="get">
+    <!-- <form class="form-group" action="site6.php" method="get">
         <input class="form-control" type="text" name="name">
+        <input class="btn btn-outline-success" value="submit" type="submit">
+    </form> -->
+    <form class="form-group" action="site6.php" method="post">
+        <input class="form-control" type="password" name="password" placeholder="Password">
         <input class="btn btn-outline-success" value="submit" type="submit">
     </form>
 
     <?php 
-        echo "<h3 class='alert alert-primary'> $_GET[name] </h3> <br/>";
+        // echo "<h3 class='alert alert-primary'> $_GET[name] </h3> <br/>";
         // http://localhost/basic/site6.php?name=Messi&age=34
         // IF WE PASS &AGE=34 IN URL WE CAN ACCESS THAT FROM HERE
-        echo $_GET["age"];
+        // echo $_GET["age"];
+
+
+        //GET VS POST
+        // WHEN USER INPUT INFORMATION THOUGH GET METHOD THAT SHOW IN URL
+        echo "<h3 class='alert alert-primary'> $_POST[password] </h3> <br/>";
     ?>
     </div>
 </body>
