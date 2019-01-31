ee7675015aba7d0f1445f4bd3d83a8ef2d1a600d
Md Shayon <mdshayon0@gmail.com>
2019-01-31 20:46:02 +0600

    basic of array

---
+++ site7.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Array</title>
+</head>
+<body>
+    <?php 
+        /*
+        // all types of data we can create in one array
+        // example: integer, string, boolean
+        $footballers = array("Messi", "Neymar", "Ronaldo", "Mbappe", "Grizmann");
+        $footballers[6] = 4000;
+        // echo $footballers[6];
+        echo count($footballers);
+        */
+
+    ?>
+</body>
+</html>
